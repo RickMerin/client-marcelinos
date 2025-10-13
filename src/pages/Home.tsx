f7d93453548427cf5fc93b4ecd2d40461f461d86
tyndarius1 <kb.dacera@mlgcl.edu.ph>
2025-10-13 23:05:14 +0800

    Kobe | Landing Page - Our Gallery Section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,17 @@
 import { BannerCarousel } from "@/components/carousels/BannerCarousel";
 import BookingForm from "@/components/forms/BookingForm";
+import ImageCarousel from '../components/imagecarousel/ImageCarousel';
 
 function Home() {
   return (
     <>
       <BannerCarousel />
       <BookingForm />
+       <ImageCarousel />
     </>
   );
 }
+
+
 
 export default Home;
