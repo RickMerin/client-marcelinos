1b5aae271d010a1f5e5caf2dd543d61f678a1c67
kylzlviii <kylzl.viii@gmail.com>
2025-10-13 23:33:07 +0800

    feat: add FAQ Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,15 @@
 import { BannerCarousel } from "@/components/carousels/BannerCarousel";
 import BookingForm from "@/components/forms/BookingForm";
+import About from "./About";
+import FAQ from "./FAQ";
 
 function Home() {
   return (
     <>
       <BannerCarousel />
-      <BookingForm />
+      <BookingForm/>
+      <About/>
+      <FAQ/>
     </>
   );
 }
--- src/routes/route.ts
+++ src/routes/route.ts
@@ -6,6 +6,7 @@ const About = lazy(() => import("../pages/About"));
 const Rooms = lazy(() => import("../pages/Rooms"));
 const NotFound = lazy(() => import("../pages/NotFound"));
 const PrivacyPolicy = lazy(() => import("../pages/PrivacyPolicy"));
+const FAQ = lazy(() => import("../pages/FAQ"));
 
 
 export const routes = [
@@ -14,6 +15,5 @@ export const routes = [
   { path: "/room", component: Rooms },
   { path: "*", component: NotFound },
   { path: "/privacy-policy", component: PrivacyPolicy },
-
-
+  { path: "/faq", component: FAQ },
 ];
