d937126b0967bdae9ea35729f846aad1b40ad850
Rick <109502961+RickMerin@users.noreply.github.com>
2026-02-26 02:45:51 +0800

    refactor: Adjust layout and styling in LocationMap component; remove unused CSS for location section

--- src/pages/Home/LocationMap.tsx
+++ src/pages/Home/LocationMap.tsx
@@ -20,42 +20,39 @@ const position: [number, number] = [10.377273056095643, 124.75196536185709];
     <div className="container mx-auto">
       <h2
         id="location-heading"
-        className="font-display text-3xl font-bold tracking-tight text-center mb-8 text-(--color-charcoal)"
-      >
-        <span className="green">FIND</span>{" "}
-        <span className="yellow">US</span>
+        className="font-display text-3xl font-bold tracking-tight text-center mb-8 text-(--color-charcoal)">
+        <span className="green">FIND</span> <span className="yellow">US</span>
       </h2>
 
-      <div className="w-full h-[70vh] rounded-[4px] overflow-hidden border border-(--color-sage-muted) shadow-md">
+      <div className="w-full h-[30vh] md:h-[60vh] rounded-[4px] overflow-hidden border border-(--color-sage-muted) shadow-md">
         <MapContainer
           center={position}
           zoom={15}
           scrollWheelZoom={false}
-          style={{ height: "100%", width: "100%" }}
-        >
+          style={{ height: "100%", width: "100%" }}>
           <TileLayer
-            attribution='© OpenStreetMap contributors'
+            attribution="© OpenStreetMap contributors"
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
 
-        <Marker position={position} icon={customIcon}>
-          <Popup>
-            <strong>Marcelino's Place</strong>
-            <br />
-            A. Mabini Street
-            <br />
-            Hilongos, Leyte
-            <br /><br />
-            <a
-              href="https://www.google.com/maps?q=10.37727432074707,124.75197963429022"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-600 underline"
-            >
-              Open in Google Maps
-            </a>
-          </Popup>
-        </Marker>
+          <Marker position={position} icon={customIcon}>
+            <Popup>
+              <strong>Marcelino's Place</strong>
+              <br />
+              A. Mabini Street
+              <br />
+              Hilongos, Leyte
+              <br />
+              <br />
+              <a
+                href="https://www.google.com/maps?q=10.37727432074707,124.75197963429022"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-600 underline">
+                Open in Google Maps
+              </a>
+            </Popup>
+          </Marker>
         </MapContainer>
       </div>
     </div>
